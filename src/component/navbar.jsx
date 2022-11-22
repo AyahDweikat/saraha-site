882bc958830d6cb058ff377a21bde6ccefacc952
Ahmadnabeelking <ahmadkhairaldin@gmail.com>
2022-11-22 18:12:46 +0200

    first commit

---
+++ src/component/navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+
+function navbar() {
+  return (
+    <>
+    <nav className="navbar navbar-expand-lg bg-custom navbar-dark bg-dark">
+     <div className="container">
+    <Link className="navbar-brand" to="home"><img src="logo-workshop.png" width={54} alt='there are img' /> </Link>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      Menu <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to="register">Register</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="login">Login</Link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+    </>
+  )
+}
+
+export default navbar
